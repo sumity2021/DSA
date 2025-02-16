c21e459a9c184a6a4b2ea7f5329c30e24a294ec5
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-02-17 04:28:17 +0530

    Time: 107 ms (91.49%) | Memory: 123.4 MB (44.45%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -6,20 +6,23 @@ public:
         vector<vector<int>>dis(n,vector<int>(m,INT_MAX));
         dis[0][0]=0;
         vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        pq.push({0,{0,0}});
-        while(!pq.empty()){
-            auto node=pq.top();
-            pq.pop();
-            int wt=node.first;
-            int row=node.second.first;
-            int col=node.second.second;
+        deque<pair<int,int>>dq;
+        dq.push_front({0,0});
+        while(!dq.empty()){
+            auto node=dq.front();
+            dq.pop_front();
+            int row=node.first;
+            int col=node.second;
             for(int i=0; i<4; i++){
                 int newrow=row+dir[i].first;
                 int newcol=col+dir[i].second;
-                if(newrow<n && newrow>=0 && newcol<m && newcol>=0 && dis[newrow][newcol]>dis[row][col]+wt){
-                    dis[newrow][newcol]=dis[row][col]+wt;
-                    pq.push({grid[newrow][newcol],{newrow,newcol}});
+                if(newrow<n && newrow>=0 && newcol<m && newcol>=0 && dis[newrow][newcol]>dis[row][col]+grid[row][col]){
+                    dis[newrow][newcol]=dis[row][col]+grid[row][col];
+                    if(grid[row][col]==1){
+                        dq.push_back({newrow,newcol});
+                    }else{
+                        dq.push_front({newrow,newcol});
+                    }
                 }
 
             }
