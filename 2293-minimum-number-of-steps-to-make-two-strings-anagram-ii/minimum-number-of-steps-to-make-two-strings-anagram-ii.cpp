dfbf51244a2d22144bcb76f1d6abcee2e11ca39f
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-02-24 22:36:19 +0530

    Time: 4 ms (97.46%) | Memory: 31.6 MB (93.66%) - LeetSync

--- 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         vector<int>freq1(26,0);
-        vector<int>freq2(26,0);
         for(auto it : s) freq1[it-'a']++;
-        for(auto it : t) freq2[it-'a']++;
+        for(auto it : t) freq1[it-'a']--;
         int ans=0;
         for(int i=0; i<26; i++){
-            ans+=abs(freq1[i]-freq2[i]);
+            ans+=abs(freq1[i]);
         }
         return ans;
     }
