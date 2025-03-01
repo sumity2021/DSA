b5dff7c589d7245334aab2bc4c6e6a74940a5a0f
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-03-01 23:03:47 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (99.53%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0; i<n-1; i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+        int last=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                last++;
+                continue;
+            }
+            nums[i-last]=nums[i];
+        }
+        for(int i=n-last; i<=n-1; i++){
+            nums[i]=0;
+        }
+        return nums;
+    }
+};
