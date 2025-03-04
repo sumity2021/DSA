5f40d5efa67e3f2e4112a60b0fb731ff559deae9
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-03-04 23:26:00 +0530

    Time: 32 ms (52.58%) | Memory: 19.1 MB (52.29%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int f(vector<int>& arr, int x, vector<int>&dp){
+        if(x<0){
+            return INT_MAX;
+        }
+        if(x==0){
+            return 0;
+        }
+        if(dp[x]!=-1){
+            return dp[x];
+        }
+        int ans=INT_MAX;
+        for(int i=0; i<arr.size(); i++){
+            ans=min(ans,f(arr,x-arr[i],dp));
+        }
+        return dp[x]=(ans==INT_MAX)? ans: ans+1;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>dp(amount+1,-1);
+        //sort(coins.begin(),coins.end());
+        int res=f(coins,amount,dp);
+        return (res==INT_MAX)? -1:res;
+    }
+};
