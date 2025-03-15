344fd3b60a587d027d435cf311cc96e6253006bd
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-03-15 21:48:42 +0530

    Time: 68 ms (80.94%) | Memory: 105.5 MB (10.78%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool f(vector<int>&dist,double hour, int speed){
+        double time_taken=0;
+        for(int i=0; i<dist.size(); i++){
+            time_taken=ceil(time_taken);
+            time_taken+=(double)dist[i]/speed;
+        }
+        return time_taken<=hour;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        if(hour<=dist.size()-1) return -1;
+        int l=1;
+        int r=1e8;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(f(dist,hour,mid)){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return l;
+
+    }
+};
