b7a1698da717e3085a3586481e7ce9496ca09d7f
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-03-16 12:37:16 +0530

    Time: 145 ms (42.09%) | Memory: 60.5 MB (11.61%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int f(vector<int>&nums, int target, int idx){
-        if(target==0 && idx==nums.size()) return 1;
-        if(idx==nums.size()) return 0;
-        int add=f(nums,target-nums[idx], idx+1);
-        int sub=f(nums,target+nums[idx], idx+1);
-        return add+sub;
+    int f(vector<int>& nums,int i, int target,vector<unordered_map<int,int>>&dp){
+        if(i==nums.size()) return target==0;
+        if(dp[i].count(target)) return dp[i][target];
+        int add=f(nums,i+1,target-nums[i],dp);
+        int sub=f(nums,i+1,target+nums[i],dp);
+        return dp[i][target]=add+sub;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(nums,target, 0);
+        vector<unordered_map<int,int>>dp(nums.size());
+        return f(nums,0,target,dp);
+
     }
 };
