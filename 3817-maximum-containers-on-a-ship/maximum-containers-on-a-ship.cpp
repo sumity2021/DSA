42fce9b2119bc29a292cfc3b9f8d84fde58e707e
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-03-23 13:57:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (41.55%) - LeetSync

--- 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.cpp
+++ 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     int maxContainers(int n, int w, int maxWeight) {
-        int total=n*n;
-        int weight=0;
-        int ans=0;
-        for(int i=0; i<total; i++){
-            weight+=w;
-            if(weight<=maxWeight){
-                ans++;
-            }
-        }
-        return ans;
+        int ans=maxWeight/w;
+        return (ans<=n*n)? ans: n*n;
     }
 };
