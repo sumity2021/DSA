3857f432e56226b2812d30f14b5be34e1ed54f5d
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-07-26 18:20:12 +0530

    Time: 692 ms (95.80%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT p.product_id,
+ROUND(IFNULL(SUM(p.price * u.units) / NULLIF(SUM(u.units), 0), 0),2) AS average_price
+FROM prices p
+LEFT JOIN unitssold u
+ON p.product_id=u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id
