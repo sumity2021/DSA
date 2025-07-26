8d7d21c95b1756e692671e3db107d1291bd138fa
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-07-26 18:41:08 +0530

    Time: 833 ms (6.15%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT p.project_id,
+ROUND(AVG(e.experience_years)*1.0,2) AS average_years
+FROM project p
+LEFT JOIN employee e
+ON p.employee_id=e.employee_id
+GROUP BY p.project_id
