3c0881622b173a602efa5048f48eaf6bd8690492
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-07-29 00:25:08 +0530

    Time: 279 ms (44.73%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,10 @@
-SELECT 
-    (SELECT DISTINCT Salary 
-     FROM Employee 
-     ORDER BY Salary DESC 
-     LIMIT 1 , 1) AS SecondHighestSalary;
+# Write your MySQL query statement below
+
+
+SELECT (
+    SELECT salary FROM (
+        SELECT * , DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk FROM employee
+    ) AS  RANKED 
+    WHERE rnk=2 
+    LIMIT 1
+) AS secondhighestsalary
