c6bb424c30bed9b2ea7c9cdbd69e676fe9b6de4a
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-07-29 15:13:19 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.81%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int last=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]==0) continue;
+            nums[last++]=nums[i];
+        }
+        for(int i=last; i<n; i++){
+            nums[i]=0;
+        }
+    }
+};
