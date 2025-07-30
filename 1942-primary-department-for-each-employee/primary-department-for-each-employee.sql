00d0b70b6f6827f703f98d7f9f2388f2358f8f38
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-07-30 23:01:38 +0530

    Time: 544 ms (54.63%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,9 +1,11 @@
 # Write your MySQL query statement below
-SELECT employee_id, department_id
-FROM employee 
-WHERE primary_flag='y' 
-OR employee_id 
-IN (SELECT employee_id
+SELECT employee_id,department_id
+FROM employee
+WHERE primary_flag='y'
+
+UNION
+
+SELECT employee_id,department_id
 FROM employee
 GROUP BY employee_id
-HAVING COUNT(*)=1)
+HAVING COUNT(*)=1
