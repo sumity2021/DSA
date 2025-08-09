15cbb14d5b7074cd184c93266874601aee4bde11
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-08-09 23:05:35 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (84.52%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int i,vector<int>&dp){
-        int n=nums.size();
-        if(i==n-1){
-            return 0;
-        }
-        if(dp[i]!=-1) return dp[i];
-        int mini=INT_MAX;
-        for(int j=1; j<=nums[i]; j++){
-            if(i+j>=n) break;
-            int jumps=f(nums,i+j,dp);
-            if(jumps!=INT_MAX){
-                mini=min(mini,jumps);
-            } 
-        }
-        return dp[i]=(mini==INT_MAX)? INT_MAX: mini+1;
-    }
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return f(nums,0,dp);
+        int jumps=0;
+        int l=0, r=0;
+        while(r<n-1){
+            int far=0;
+            for(int i=l; i<=r; i++){
+                far=max(far,i+nums[i]);
+            }
+            l=r+1;
+            r=far;
+            jumps++;
+        }
+        return jumps;
     }
 };
