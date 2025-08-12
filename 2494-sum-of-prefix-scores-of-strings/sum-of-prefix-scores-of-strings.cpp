425b5583b65231c07b4b19f5e305590c8cdf905b
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-08-12 14:04:30 +0530

    Time: 676 ms (40.29%) | Memory: 720 MB (22.30%) - LeetSync

---
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,68 @@
+struct Node{
+    Node*links[26];
+    bool flag=false;
+    int freq=0;
+    bool containskey(char ch){
+        return links[ch-'a']!=NULL;
+    }
+    void inc(){
+        freq++;
+    }
+    void put(char ch , Node*node){
+        links[ch-'a']=node;
+    }
+    Node*get(char ch){
+        return links[ch-'a'];
+    }
+    void setend(){
+        flag=true;
+    }
+    bool isend(){
+        return flag;
+    }
+    int char_freq(){
+        return freq;
+    }
+};
+class Trie{
+    Node*root;
+public:
+    Trie(){
+        root=new Node();
+    }
+    void insert(string word){
+        Node*node=root;
+        for(int i=0; i<word.size(); i++){
+            if(!node->containskey(word[i])){
+                node->put(word[i],new Node());
+            }
+            node=node->get(word[i]);
+            node->inc();
+        }
+        node->setend();
+    }
+    int prefix(string word){
+        Node*node=root;
+        int cnt=0;
+        for(int i=0; i<word.size(); i++){
+            node=node->get(word[i]);
+            cnt+=node->char_freq();
+        }
+        return cnt;
+    }
+};
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        int n=words.size();
+        Trie trie;
+        for(int i=0; i<n; i++){
+            trie.insert(words[i]);
+        }
+        vector<int>ans;
+        for(int i=0; i<n; i++){
+            ans.push_back(trie.prefix(words[i]));
+        }
+        return ans;
+    }
+};
