cced57627905da3a098d7c1ef1485c2b2d854eb5
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-08-31 01:56:32 +0530

    Time: 23 ms (76.35%) | Memory: 26 MB (88.76%) - LeetSync

--- 1543-simplified-fractions/simplified-fractions.cpp
+++ 1543-simplified-fractions/simplified-fractions.cpp
@@ -13,8 +13,7 @@ public:
         for(int i=2; i<=n; i++){
             for(int j=1; j<i; j++){
                 if(gcd(i,j)==1){
-                    string ele=to_string(j)+"/"+to_string(i);
-                    ans.push_back(ele);
+                    ans.push_back(to_string(j)+"/"+to_string(i));
                 }
             }
         }
