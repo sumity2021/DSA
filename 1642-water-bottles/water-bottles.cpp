fbdfab84133353fedd1028be20b8b8b013ff085b
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-10-02 00:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (80.28%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    int numWaterBottles(int n, int e) {
-        int total=0;
-        int rem=0;
-        while(n){
-            total+=n;
-            int nb=(n+rem)/e;
-            rem=(n+rem)%e;
-            n=nb;
-        }
-        return total;
+    int numWaterBottles(int b, int e) {
+        return (b*e-1)/(e-1);
     }
 };
