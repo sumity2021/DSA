177081ff9204795dafcd142fdccd1e2a0c999795
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-10-19 23:41:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (78.91%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int l=0,r=n-1,t=0,b=m-1;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int l=0,r=m-1,t=0,b=n-1;
         vector<int>ans;
-        while(r>=l && b>=t){
+        while(l<=r && t<=b){
             for(int i=l; i<=r; i++){
                 ans.push_back(matrix[t][i]);
             }
             t++;
-            for(int j=t; j<=b; j++){
-                ans.push_back(matrix[j][r]);
+            for(int i=t; i<=b; i++ ){
+                ans.push_back(matrix[i][r]);
             }
             r--;
-            for(int i=r; i>=l && t<=b; i--){
-                ans.push_back(matrix[b][i]);
+            if(t<=b){
+                for(int i=r; i>=l; i--){
+                    ans.push_back(matrix[b][i]);
+                }
             }
             b--;
-            for(int j=b; j>=t && r>=l; j--){
-                ans.push_back(matrix[j][l]);
+            if(l<=r){
+                for(int i=b; i>=t; i--){
+                    ans.push_back(matrix[i][l]);
+                }
             }
             l++;
         }
