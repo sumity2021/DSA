c9a11e31b60da7b26e99e3156482745b52e5eac6
SUMIT <125662563+sumity2021@users.noreply.github.com>
2025-11-05 09:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (86.70%) - LeetSync

--- 2309-maximize-number-of-subsequences-in-a-string/maximize-number-of-subsequences-in-a-string.cpp
+++ 2309-maximize-number-of-subsequences-in-a-string/maximize-number-of-subsequences-in-a-string.cpp
@@ -3,13 +3,16 @@ public:
     long long maximumSubsequenceCount(string text, string pattern) {
         int n=text.size();
         long long ans=0;
-        unordered_map<char,int>m;
+        int st=0,end=0;
         for(int i=0; i<n; i++){
             if(text[i]==pattern[1]){
-                ans+=m[pattern[0]];
+                ans+=st;
+                end++;
             }
-            m[text[i]]++;
+            if(pattern[0]==text[i]){
+                st++;
+            }
         }
-        return ans+max(m[pattern[0]],m[pattern[1]]);
+        return ans+max(st,end);
     }
 };
